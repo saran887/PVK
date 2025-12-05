6fc5ab461d063a7d5ebc3aa8b9134ba383ea8c49
saran887 <saransarvesh213@gmail.com>
2025-12-05 19:36:36 +0530

    Configure Firebase API keys in separate local file (ignored by git)

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'firebase_options_local.dart';
 import 'core/router/app_router.dart';
 import 'core/theme/app_theme.dart';
 
